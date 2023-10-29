a77ebb9bdf82384861a92d8fd59fbaf8410b51d9
shmulisarmy <shmulikeller@gmail.com>
2023-10-29 10:45:06 -0400

    first losson

---
+++ timestable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int tableSize = 10;
+    for (int i = 1; i <= tableSize; i++){
+        printf("\n");
+    for (int j = 1; j <= tableSize; j++){
+        int number = i * j;
+
+        printf("%4d", number);
+    }}
+    }
